f14b39b4c14527d9458bcda32ec9e8862a87df6e
Loïc <lolo.mess.17@gmail.com>
2018-04-26 16:09:02 +0200

    Modif inscription

--- Inscription.php
+++ Inscription.php
@@ -29,41 +29,42 @@ if($_POST)
 ?>
 <?php require_once("inc/haut.inc.php"); ?>
 <?php echo $contenu; ?>
-<head>
-    <title>FREE SK8 - Inscription</title>
-    <link rel="stylesheet" href="HomePage.css">
-    <link rel="stylesheet" href="inscription.css">
-</head>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <label for="pseudo">Pseudo</label><br>
-    <input type="text" id="pseudo" name="pseudo" maxlength="20" placeholder="votre pseudo" pattern="[a-zA-Z0-9-_.]{1,20}" title="caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
-         
-    <label for="mdp">Mot de passe</label><br>
-    <input type="password" id="mdp" name="mdp" required="required"><br><br>
-         
-    <label for="nom">Nom</label><br>
-    <input type="text" id="nom" name="nom" placeholder="votre nom"><br><br>
-         
-    <label for="prenom">Prénom</label><br>
-    <input type="text" id="prenom" name="prenom" placeholder="votre prénom"><br><br>
- 
-    <label for="email">Email</label><br>
-    <input type="email" id="email" name="email" placeholder="exemple@gmail.com"><br><br>
-         
-    <label for="civilite">Civilité</label><br>
-    <input name="civilite" value="m" checked="" type="radio">Homme
-    <input name="civilite" value="f" type="radio">Femme<br><br>
-                 
-    <label for="ville">Ville</label><br>
-    <input type="text" id="ville" name="ville" placeholder="votre ville" pattern="[a-zA-Z0-9-_.]{5,15}" title="caractères acceptés : a-zA-Z0-9-_."><br><br>
-         
-    <label for="cp">Code Postal</label><br>
-    <input type="text" id="code_postal" name="code_postal" placeholder="code postal" pattern="[0-9]{5}" title="5 chiffres requis : 0-9"><br><br>
-         
-    <label for="adresse">Adresse</label><br>
-    <textarea id="adresse" name="adresse" placeholder="votre dresse" pattern="[a-zA-Z0-9-_.]{5,15}" title="caractères acceptés :  a-zA-Z0-9-_."></textarea><br><br>
- 
-    <input name="inscription" value="S'inscrire" type="submit">
-</form>
+
+<html>
+    <head>
+        <title>Inscription - FreeSK8</title>
+    </head>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="pseudo">Pseudo</label><br>
+        <input type="text" id="pseudo" name="pseudo" maxlength="20" placeholder="votre pseudo" pattern="[a-zA-Z0-9-_.]{1,20}" title="caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
+
+        <label for="mdp">Mot de passe</label><br>
+        <input type="password" id="mdp" name="mdp" required="required"><br><br>
+
+        <label for="nom">Nom</label><br>
+        <input type="text" id="nom" name="nom" placeholder="votre nom" required="required"><br><br>
+
+        <label for="prenom">Prénom</label><br>
+        <input type="text" id="prenom" name="prenom" placeholder="votre prénom" required="required"><br><br>
+
+        <label for="email">Email</label><br>
+        <input type="email" id="email" name="email" placeholder="exemple@gmail.com" required="required"><br><br>
+
+        <label>Civilité</label><br>
+        <input class="sexe" name="sexe" value="m" checked="" type="radio">Homme
+        <input class="sexe" name="sexe" value="f" type="radio">Femme<br><br>
+
+        <label for="ville">Ville</label><br>
+        <input type="text" id="ville" name="ville" required="required" placeholder="votre ville" pattern="[a-zA-Z0-9-_.]{5,15}" title="caractères acceptés : a-zA-Z0-9-_."><br><br>
+
+        <label for="code_postal">Code Postal</label><br>
+        <input type="text" id="code_postal" name="code_postal" required="required" placeholder="code postal" pattern="[0-9]{5}" title="5 chiffres requis : 0-9"><br><br>
+
+        <label for="adresse">Adresse</label><br>
+        <textarea id="adresse" name="adresse" required="required" placeholder="votre dresse" pattern="[a-zA-Z0-9-_.]{5,15}" title="caractères acceptés :  a-zA-Z0-9-_."></textarea><br><br>
+
+        <input name="inscription" value="S'inscrire" type="submit">
+    </form>
+</html>
  
 <?php require_once("inc/bas.inc.php"); ?>
--- inc/bas.inc.php
+++ inc/bas.inc.php
@@ -2,7 +2,7 @@
         </section>
         <footer>
             <div class="conteneur">    
-                <?php echo date('Y'); ?> - Tous droits reservés - MonNom MonPrenom.
+                <?php echo date('Y'); ?> - Tous droits reservés - FreeSK8.
             </div>
         </footer>
     </body>
