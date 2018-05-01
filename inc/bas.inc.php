217ff322a4c32e8fbe85e4a9ff42ab7c36fc9c97
Loïc <lolo.mess.17@gmail.com>
2018-05-01 17:38:51 +0200

    Suppression avant réupload

--- inc/bas.inc.php
+++
@@ -1,9 +0,0 @@
-            </div>
-        </section>
-        <footer>
-            <div class="conteneur">    
-                <?php echo date('Y'); ?> - Tous droits reservés - FreeSK8.
-            </div>
-        </footer>
-    </body>
-</html>
